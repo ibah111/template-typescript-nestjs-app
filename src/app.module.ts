15a87d62fc264a8131f73d9648f41531d1188375
ibah <109962513+ibah111@users.noreply.github.com>
2025-03-02 19:41:45 +0300

    feat: page and module modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import ModulesModule from './modules';
+import PagesModule from './pages';
 
 @Module({
-  imports: [],
+  imports: [ModulesModule, PagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
