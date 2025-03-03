9c1f7c5f104e55adf4eef5eb616f022511b66a53
ibah <starlord77@mail.ru>
2025-03-03 16:58:27 +0300

    feat(adset): adset controller + service add + full_tree methods

--- src/pages/adset/adset.controller.ts
+++ src/pages/adset/adset.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import AdsetService from './adset.service';
-import { AdsetInput } from './adset.input';
+import { AdsetInput, RegionNameInput } from './adset.input';
 
 @ApiTags('adset')
 @Controller('adset')
@@ -10,12 +10,18 @@ export default class AdsetController {
 
   @Get('config')
   async config(@Query() input: AdsetInput) {
-    console.log(
-      'config: '.yellow,
-      `geo: ${input.geo}, device: ${input.device}`,
-    );
     return await this.service.generateAdSet({
       ...input,
     });
+  }
+
+  @Get('full_tree')
+  async full_tree() {
+    return await this.service.fulltree();
+  }
+
+  @Post('add_geo')
+  async add_geo(@Body() { region }: RegionNameInput) {
+    return await this.service.addgeo(region);
   }
 }
--- src/pages/adset/adset.service.ts
+++ src/pages/adset/adset.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { AdsetInput } from './adset.input';
+import { AdsetInput, RegionNameInput } from './adset.input';
 import { AdSet, Tree } from './adset.dto/adset.interface';
 import * as tree from '../../tree.json';
+import { InjectModel } from '@nestjs/sequelize';
+import Geolocation from 'src/modules/databases/sqlite/models/geolocation.model';
+import Module from 'src/modules/databases/sqlite/models/module.model';
+import Monetization from 'src/modules/databases/sqlite/models/monetization.model';
+import MonetizationOption from 'src/modules/databases/sqlite/models/monetization.option.model';
+import Push from 'src/modules/databases/sqlite/models/push.model';
+import PushOption from 'src/modules/databases/sqlite/models/push.option.model';
 
 @Injectable()
 export default class AdsetService {
+  constructor(
+    @InjectModel(Geolocation, 'sqlite')
+    private readonly modelGeo: typeof Geolocation,
+  ) {}
+
   private readonly tree: Tree = tree;
 
   private selectNode(options: { name: string; probability: number }[]): string {
@@ -39,5 +51,75 @@ export default class AdsetService {
     }
 
     return adSet;
+  }
+
+  async fulltree() {
+    const all_geo = await this.modelGeo.findAll({
+      attributes: ['id', 'region_name', 'probability'],
+      include: [
+        {
+          model: Module,
+          include: [
+            {
+              model: Monetization,
+              include: [
+                {
+                  model: MonetizationOption,
+                },
+              ],
+            },
+            {
+              model: Push,
+              include: [
+                {
+                  model: PushOption,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    return all_geo;
+  }
+
+  async addgeo(region: string) {
+    let probability: number = 100;
+    const region_name = region.toLocaleUpperCase();
+    const all_geolocations = await this.modelGeo.findAll();
+    console.log(
+      'ALL'.yellow,
+      all_geolocations.map((res) => res.dataValues),
+    );
+    const total_count = all_geolocations.length;
+    if (total_count > 0) {
+      probability = probability / total_count;
+      console.log(region_name, probability);
+      for (const geo of all_geolocations) {
+        await geo
+          .update({
+            probability,
+          })
+          .then((res) => {
+            console.log(
+              'Probability of Region ' + `${res.region_name}`.yellow,
+              ' been updated to ' + `${probability}`.yellow,
+            );
+          });
+      }
+    }
+    return await this.modelGeo.create({
+      region_name,
+      probability,
+    });
+  }
+
+  async deletegeo({ region }: RegionNameInput) {
+    const region_name = region.toLocaleUpperCase();
+    return await this.modelGeo.destroy({
+      where: {
+        region_name,
+      },
+    });
   }
 }
