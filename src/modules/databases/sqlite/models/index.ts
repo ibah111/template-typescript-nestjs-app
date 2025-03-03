ea2a34a101217685ccf0c3c5c597c436b57167bd
ibah <starlord77@mail.ru>
2025-03-03 15:10:36 +0300

    feat(db-models): combined in common pool + server launch with 0 errors

--- src/modules/databases/sqlite/models/index.ts
+++ src/modules/databases/sqlite/models/index.ts
@@ -1,3 +1,16 @@
 import { ModelCtor } from 'sequelize-typescript';
+import Module from './module.model';
+import Monetization from './monetization.model';
+import PushOption from './push.option.model';
+import Geolocation from './geolocation.model';
+import Push from './push.model';
+import MonetizationOption from './monetization.option.model';
 
-export const models: ModelCtor[] = [];
+export const models: ModelCtor[] = [
+  Module,
+  Monetization,
+  MonetizationOption,
+  Push,
+  PushOption,
+  Geolocation,
+];
