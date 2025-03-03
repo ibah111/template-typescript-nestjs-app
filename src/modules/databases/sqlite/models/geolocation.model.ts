56f154caabef9f71bd50fc554e2668ecb77c3bd5
ibah <starlord77@mail.ru>
2025-03-03 15:26:39 +0300

    fix(db-models): adding allowNull = false

--- src/modules/databases/sqlite/models/geolocation.model.ts
+++ src/modules/databases/sqlite/models/geolocation.model.ts
@@ -1,5 +1,12 @@
 import { InferAttributes, InferCreationAttributes } from 'sequelize';
-import { Column, DataType, HasMany, Model, Table } from 'sequelize-typescript';
+import {
+  AllowNull,
+  Column,
+  DataType,
+  HasMany,
+  Model,
+  Table,
+} from 'sequelize-typescript';
 import Module from './module.model';
 
 @Table({
@@ -10,9 +17,11 @@ export default class Geolocation extends Model<
   InferAttributes<Geolocation>,
   InferCreationAttributes<Geolocation>
 > {
+  @AllowNull(false)
   @Column(DataType.STRING)
   region_name: string;
 
+  @AllowNull(false)
   @Column(DataType.FLOAT)
   probability: number;
 
