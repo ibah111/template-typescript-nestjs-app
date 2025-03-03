35664be10e798fda41e14d11d66670114fbed105
ibah <starlord77@mail.ru>
2025-03-03 15:32:50 +0300

    feat(adset-page): added db models to adset page module

--- src/pages/adset/adset.module.ts
+++ src/pages/adset/adset.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import AdsetService from './adset.service';
 import AdsetController from './adset.controller';
-
+import { SequelizeModule } from '@nestjs/sequelize';
+import Monetization from 'src/modules/databases/sqlite/models/monetization.model';
+import MonetizationOption from 'src/modules/databases/sqlite/models/monetization.option.model';
+import Push from 'src/modules/databases/sqlite/models/push.model';
+import PushOption from 'src/modules/databases/sqlite/models/push.option.model';
+import ModuleModel from 'src/modules/databases/sqlite/models/module.model';
+import Geolocation from 'src/modules/databases/sqlite/models/geolocation.model';
 @Module({
+  imports: [
+    SequelizeModule.forFeature(
+      [
+        ModuleModel,
+        Monetization,
+        MonetizationOption,
+        Push,
+        PushOption,
+        Geolocation,
+      ],
+      'sqlite',
+    ),
+  ],
   controllers: [AdsetController],
   providers: [AdsetService],
 })
