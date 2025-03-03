1701b3029a9634d95929a90e26be2fef3d3cc8c1
ibah <starlord77@mail.ru>
2025-03-03 15:09:49 +0300

    feat(db-models): monetization models + option

---
+++ src/modules/databases/sqlite/models/monetization.model.ts
@@ -0,0 +1,37 @@
+import { InferAttributes, InferCreationAttributes } from 'sequelize';
+import {
+  AllowNull,
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  HasMany,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import Geolocation from './geolocation.model';
+import Module from './module.model';
+import MonetizationOption from './monetization.option.model';
+
+@Table({
+  paranoid: true,
+  tableName: 'Monetizations',
+})
+export default class Monetization extends Model<
+  InferAttributes<Monetization>,
+  InferCreationAttributes<Monetization>
+> {
+  @ForeignKey(() => Module)
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  r_module_id: number;
+
+  @BelongsTo(() => Module)
+  module: Module;
+
+  @Column(DataType.FLOAT)
+  probability: number;
+
+  @HasMany(()=> MonetizationOption)
+  options: MonetizationOption;
+}
---
+++ src/modules/databases/sqlite/models/monetization.option.model.ts
@@ -0,0 +1,36 @@
+import { InferAttributes, InferCreationAttributes } from 'sequelize';
+import {
+  AllowNull,
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import Monetization from './monetization.model';
+
+@Table({
+  paranoid: true,
+  tableName: 'MonetizationOptions',
+})
+export default class MonetizationOption extends Model<
+  InferAttributes<MonetizationOption>,
+  InferCreationAttributes<MonetizationOption>
+> {
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  name: string;
+
+  @AllowNull(false)
+  @Column(DataType.FLOAT)
+  probability: number;
+
+  @ForeignKey(() => Monetization)
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  r_monetization_id: number;
+
+  @BelongsTo(() => Monetization)
+  monetization: Monetization;
+}
