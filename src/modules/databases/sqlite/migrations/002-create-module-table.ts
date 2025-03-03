aea76729e199b8e7f77a80c3cec9c1e052b7982b
ibah <starlord77@mail.ru>
2025-03-03 15:27:15 +0300

    feat(migations): module migration

---
+++ src/modules/databases/sqlite/migrations/002-create-module-table.ts
@@ -0,0 +1,42 @@
+import { DataTypes, QueryInterface } from 'sequelize';
+import { MigrationFn } from 'umzug';
+
+const tableName = 'modules';
+
+export const up: MigrationFn<QueryInterface> = ({ context }) =>
+  context.sequelize.transaction((t) =>
+    Promise.all([
+      context.createTable(
+        tableName,
+        {
+          id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          r_geo_id: {
+            type: DataTypes.STRING,
+            references: {
+              model: 'geolocations',
+              key: 'id',
+            },
+            onDelete: 'CASCADE',
+          },
+          createdAt: {
+            allowNull: false,
+            type: DataTypes.DATE,
+          },
+          updatedAt: {
+            allowNull: false,
+            type: DataTypes.DATE,
+          },
+        },
+        { transaction: t },
+      ),
+    ]),
+  );
+export const down: MigrationFn<QueryInterface> = ({ context }) =>
+  context.sequelize.transaction((t) =>
+    Promise.all([context.dropTable(tableName, { transaction: t })]),
+  );
