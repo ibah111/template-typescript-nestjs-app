289aa13dc6c4546d4d801fc9cb691248d7e58b54
ibah <starlord77@mail.ru>
2025-03-03 15:09:12 +0300

    feat(db-models): module model

---
+++ src/modules/databases/sqlite/models/module.model.ts
@@ -0,0 +1,37 @@
+import { InferAttributes, InferCreationAttributes } from 'sequelize';
+import {
+  AllowNull,
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  HasMany,
+  HasOne,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import Geolocation from './geolocation.model';
+import Push from './push.model';
+import Monetization from './monetization.model';
+
+@Table({
+  paranoid: true,
+  tableName: 'Modules',
+})
+export default class Module extends Model<
+  InferAttributes<Module>,
+  InferCreationAttributes<Module>
+> {
+  @ForeignKey(() => Geolocation)
+  @Column(DataType.INTEGER)
+  r_geo_id: number;
+
+  @BelongsTo(() => Geolocation)
+  geolocation: Geolocation;
+
+  @HasOne(() => Push)
+  Pushs: Push;
+
+  @HasOne(() => Monetization)
+  Monetizations: Monetization;
+}
