1cde90a6b2e0eabfc822579661b2496a8f3d3e30
ibah <starlord77@mail.ru>
2025-03-03 15:10:13 +0300

    feat(db-models): push model + option

---
+++ src/modules/databases/sqlite/models/push.model.ts
@@ -0,0 +1,32 @@
+import {
+  AllowNull,
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  HasMany,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import Module from './module.model';
+import PushOption from './push.option.model';
+
+@Table({
+  paranoid: true,
+  tableName: 'Pushs',
+})
+export default class Push extends Model {
+  @ForeignKey(() => Module)
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  r_module_id: number;
+
+  @BelongsTo(() => Module)
+  module: Module;
+
+  @Column(DataType.FLOAT)
+  probability: number;
+
+  @HasMany(() => PushOption)
+  options: PushOption[];
+}
