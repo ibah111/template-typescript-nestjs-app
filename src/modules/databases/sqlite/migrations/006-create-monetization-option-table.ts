06598a48a08936526da4e92812c0c4d322794a21
ibah <starlord77@mail.ru>
2025-03-03 16:11:30 +0300

    feat(migrations): migrations + option table

---
+++ src/modules/databases/sqlite/migrations/006-create-monetization-option-table.ts
@@ -0,0 +1,51 @@
+import { DataTypes, QueryInterface } from 'sequelize';
+import { MigrationFn } from 'umzug';
+
+const tableName = 'MonetizationOptions';
+
+export const up: MigrationFn<QueryInterface> = ({ context }) =>
+  context.sequelize.transaction((t) =>
+    Promise.all([
+      context.createTable(
+        tableName,
+        {
+          id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          probability: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          r_monetization_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+              model: 'Monetizations',
+              key: 'id',
+            },
+            onDelete: 'CASCADE',
+          },
+          createdAt: {
+            allowNull: false,
+            type: DataTypes.DATE,
+          },
+          updatedAt: {
+            allowNull: false,
+            type: DataTypes.DATE,
+          },
+        },
+        { transaction: t },
+      ),
+    ]),
+  );
+export const down: MigrationFn<QueryInterface> = ({ context }) =>
+  context.sequelize.transaction((t) =>
+    Promise.all([context.dropTable(tableName, { transaction: t })]),
+  );
