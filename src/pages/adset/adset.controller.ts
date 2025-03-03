68bda000edd9e7359a05d3a5af19e5894f54029b
ibah <starlord77@mail.ru>
2025-03-03 10:09:31 +0300

    feat(adset): controller remake

--- src/pages/adset/adset.controller.ts
+++ src/pages/adset/adset.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import AdsetService from './adset.service';
+import { AdsetInput } from './adset.input';
 
 @ApiTags('adset')
 @Controller('adset')
@@ -8,11 +9,13 @@ export default class AdsetController {
   constructor(private readonly service: AdsetService) {}
 
   @Get('config')
-  async config(@Query('geo') geo: string, @Query('device') device: string) {
-    console.log('config: '.yellow, `geo: ${geo}, device: ${device}`);
+  async config(@Query() input: AdsetInput) {
+    console.log(
+      'config: '.yellow,
+      `geo: ${input.geo}, device: ${input.device}`,
+    );
     return await this.service.config({
-      geo,
-      device,
+      ...input,
     });
   }
 }
